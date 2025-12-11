7c7157f95b488d939c91dbd693d40501c7f2aa88
Akshay Kwatra <Akshay.Kwatra@tesco.com>
2025-12-11 12:09:52 +0530

    initialized spring boot project with gradle and required dependencies

---
+++ src/main/java/com/santa4you/santa_delivery_service/Santa4YouChristmasDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.santa4you.santa_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santa4YouChristmasDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santa4YouChristmasDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santa4you/santa_delivery_service/Santa4YouChristmasDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santa4you.santa_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santa4YouChristmasDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
