80ab7eda6f97c30fa9427f8dc4187466e654a60d
Akshay Kwatra <akki_10@Akshays-MacBook-Pro.local>
2025-12-13 02:44:02 +0530

    add user repository

---
+++ src/main/java/com/santa4you/santa_delivery_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.santa4you.santa_delivery_service.repository;
+
+import com.santa4you.santa_delivery_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
