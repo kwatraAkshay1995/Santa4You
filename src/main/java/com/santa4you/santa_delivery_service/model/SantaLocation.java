fea10fdb9d1e6a5f5d9d565bdefbc53bafae9132
Akshay Kwatra <akki_10@Akshays-MacBook-Pro.local>
2025-12-13 03:05:28 +0530

    add santa location model

---
+++ src/main/java/com/santa4you/santa_delivery_service/model/SantaLocation.java
@@ -0,0 +1,16 @@
+package com.santa4you.santa_delivery_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SantaLocation {
+    private String city;
+    private String country;
+    private double latitude;
+    private double longitude;
+    private String estimatedArrival;
+}
